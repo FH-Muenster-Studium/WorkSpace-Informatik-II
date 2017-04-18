cfee68d107a3485f838a1e47ec56da6b539fc12c
fabianterhorst <fabian.terhorst@gmail.com>
2017-04-18 18:50:42 +0200

    Add error message when load failes

--- Praktikum1/src/Application.java
+++ Praktikum1/src/Application.java
@@ -14,6 +14,7 @@ public class Application {
             loadFile(vocablesFile);
         } catch (IOException io) {
             io.printStackTrace();
+            System.out.println("Fehler beim Laden der Vokabeln");
         }
     }};
 
